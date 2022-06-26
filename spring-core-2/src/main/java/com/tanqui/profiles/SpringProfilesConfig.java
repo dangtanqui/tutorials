326eb99ca989aa47558197dfd990dfbcccf44d57
dangtanqui <quidang9656@gmail.com>
2022-06-26 17:05:10 +0700

    spring-core-2 profiles update

--- spring-core-2/src/main/java/com/tanqui/profiles/SpringProfilesConfig.java
+++ spring-core-2/src/main/java/com/tanqui/profiles/SpringProfilesConfig.java
@@ -3,9 +3,14 @@ package com.tanqui.profiles;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @ComponentScan("com.tanqui.profiles")
-@PropertySource({"classpath:application.properties", "classpath:application-${spring.profiles.active}.properties"})
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:application-${spring.profiles.active}.properties")
+})
+//@PropertySource({"classpath:application.properties", "classpath:application-${spring.profiles.active}.properties"})
 public class SpringProfilesConfig {
 }
